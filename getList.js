888039b513c0bd4a39fd8bf9c1d66757cafd06fa
maxym <maxym.marchenko@gmail.com>
2016-10-04 17:48:59 +0300

    пишу в базу номера обявлений

--- getList.js
+++ getList.js
@@ -47,76 +47,76 @@ function getInfo(callback){
     var res = ''
     var fs = require('fs');
     fs.readFile('response.txt', 'utf8', function (err,data) {
-      if (err) {
-        return (function(){
-            console.log(err);
+        if (err) {
+            return (function(){
+                console.log(err);
 
-            var params = {
-                "category_id" : 1,
-                "marka_id[0]" : 0,
-                "model_id[0]" : 0,
-                "s_yers[0]" : 2010,
-                "po_yers[0]" : 2016,
-                "price_do" : 12500,
-                "currency" : 1,
-                "state[0]" : 7,
-                "state[1]" : 8,
-                "state[2]" : 20,
-                "custom" : 1,
-                "under_credit" : 2,
-                "confiscated_car" : 2,
-                "damage" : 1,
-                "auto_repairs" : 2,
-                "engineVolumeFrom" : "1.3",
-                "engineVolumeTo" : "",
-                "power_name" : 1,
-                "fuelRatesType" : "city",
-                "gearbox[1]" : 2,
-                "gearbox[2]" : 3,
-                "gearbox[3]" : 4,
-                "gearbox[4]" : 5,
-                "top" : 10,
-                "countpage" : 2,
-                "page" : 0
+                var params = {
+                    "category_id" : 1,
+                    "marka_id[0]" : 0,
+                    "model_id[0]" : 0,
+                    "s_yers[0]" : 2010,
+                    "po_yers[0]" : 2016,
+                    "price_do" : 12500,
+                    "currency" : 1,
+                    "state[0]" : 7,
+                    "state[1]" : 8,
+                    "state[2]" : 20,
+                    "custom" : 1,
+                    "under_credit" : 2,
+                    "confiscated_car" : 2,
+                    "damage" : 1,
+                    "auto_repairs" : 2,
+                    "engineVolumeFrom" : "1.3",
+                    "engineVolumeTo" : "",
+                    "power_name" : 1,
+                    "fuelRatesType" : "city",
+                    "gearbox[1]" : 2,
+                    "gearbox[2]" : 3,
+                    "gearbox[3]" : 4,
+                    "gearbox[4]" : 5,
+                    "top" : 10,
+                    "countpage" : 2,
+                    "page" : 0
 
-            }
+                }
 
-            var options = {
+                var options = {
 
-                host: 's-ua.auto.ria.com',
-                port: 443,
-                path: '/blocks_search_ajax/search/?' + prepareParams(params),
-                method: 'GET'
-            };
-            var response = '';
-            var http = require('https');
-            // console.log('STATUS: ');
-            var req = http.request(options, function(res) {
+                    host: 's-ua.auto.ria.com',
+                    port: 443,
+                    path: '/blocks_search_ajax/search/?' + prepareParams(params),
+                    method: 'GET'
+                };
+                var response = '';
+                var http = require('https');
+                // console.log('STATUS: ');
+                var req = http.request(options, function(res) {
 
-                // console.log('HEADERS: ' + JSON.stringify(res.headers));
-                res.setEncoding('utf8');
-                res.on('data', function (chunk) {
-                    response += chunk;        
+                    // console.log('HEADERS: ' + JSON.stringify(res.headers));
+                    res.setEncoding('utf8');
+                    res.on('data', function (chunk) {
+                        response += chunk;        
+                    });
+                    res.on('end', function () {
+                        saveResponce(response);
+                        //processAll(response);
+                        getInfo();
+
+                    });    
                 });
-                res.on('end', function () {
-                    saveResponce(response);
-                    //processAll(response);
-                    getInfo(callback);
 
-                });    
-            });
-
-            req.on('error', function(e) {
-                console.log('problem with request: ' + e.message);
-            });
-            req.end();        
-        })();
-      }
-      // getIds(data);
+                req.on('error', function(e) {
+                    console.log('problem with request: ' + e.message);
+                });
+                req.end();        
+            })();
+        }
+        
         var blurbs = getIds(data);
 
         for (var i = 0; i < blurbs.length; i++) {
-            // console.log(parseInt(blurbs[i]));
+            console.log('ggg');
             getAdvDetailes(parseInt(blurbs[i]));
             
         }
